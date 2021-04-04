3c1901536bbd56ab34a1b32a901bf0935b1cd35e
BiscuitButB <76254591+7Biscuits@users.noreply.github.com>
2021-04-04 22:35:39 +0530

    Create calc.js

---
+++ calc.js
@@ -0,0 +1,27 @@
+<script>
+
+   var num_1 = parseFloat(prompt("Enter the first number"));
+   var num_2 = parseFloat(prompt("Enter the second number"));
+   var operator = prompt("Enter the operator");
+
+   let result;
+
+   if(operator == "+") {
+       result = num_1 + num_2;
+   }
+   else if(operator == "-") {
+       result == num_1 - num_2;
+   }
+   else if(operator == "*") {
+       result == num_1 * num_2;
+   }
+   else if(operator == "/") {
+       result == num_1 / num_2;
+   } 
+   else {
+       result = "Invalid Input"
+   }
+   console.log(result)
+   
+
+</script>
